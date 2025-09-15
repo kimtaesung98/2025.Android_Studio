4be093c4c2195dc5b05f00356443efec007ddbc3
rlaxotjd <xlavh25576@gmail.com>
2025-09-15 21:02:18 +0900

    android file

---
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.practice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val tv: TextView = findViewById(R.id.tv_hello)
+        tv.text  = "안녕"
+
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+        }
+    }
+}
